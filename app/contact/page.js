c31a6050b339bd928df6e4927875b964a7249463
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-19 21:34:24 -0400

    change contact form theme to black

--- app/contact/page.js
+++ app/contact/page.js
@@ -80,17 +80,17 @@ export default function Contact() {
         <ul>
           <li>
             <a href="https://www.linkedin.com/in/weijiew/">
-              <i className="bx bxl-linkedin"></i>
+              <i className="bx bxl-linkedin" style={{ color: "#eeeeee" }}></i>
             </a>
           </li>
           <li>
             <a href="https://github.com/iam-weijie">
-              <i className="bx bxl-github"></i>
+              <i className="bx bxl-github" style={{ color: "#eeeeee" }}></i>
             </a>
           </li>
           <li>
             <a href="https://x.com/iam_weijie">
-              <i class="bx bxl-twitter"></i>
+              <i className="bx bxl-twitter" style={{ color: "#eeeeee" }}></i>
             </a>
           </li>
         </ul>
